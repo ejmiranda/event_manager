7901eaaa5dd1a1a8ab5269d655ad2fe8ecf36a39
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-05-30 09:03:07 -0500

    Complete until 'Using ERB'

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,7 @@
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def format_zipcode(zipcode, digits)
   zipcode.to_s.rjust(digits, '0')[0..digits - 1]
@@ -11,12 +12,11 @@ def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators.officials.map(&:name).join(', ')
+    ).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
@@ -30,15 +30,14 @@ content = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
 content.each do |row|
   name = row[:first_name]
   zipcode = format_zipcode(row[:zipcode], 5)
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-
-  puts personal_letter
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
