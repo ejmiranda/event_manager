0db0d4317697326e5b469b40a2feafa6e8c527f3
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-05-30 09:13:31 -0500

    Complete until 'Moving form letter generation to a method'

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -22,6 +22,14 @@ def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'EventManager initialized.'
 
 content = CSV.open(
@@ -34,10 +42,12 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 content.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = format_zipcode(row[:zipcode], 5)
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
+
+  save_thank_you_letter(id, form_letter)
 end
