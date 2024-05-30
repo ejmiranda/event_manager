980625a72cd2818d4c859c214dba46ce4878aaa7
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2024-05-30 08:42:27 -0500

    Complete until 'Our template system has problems'

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,44 @@
-puts 'Event Manager Initialized!'
+# frozen_string_literal: true
+
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def format_zipcode(zipcode, digits)
+  zipcode.to_s.rjust(digits, '0')[0..digits - 1]
+end
+
+def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators.officials.map(&:name).join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
+
+content = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.html')
+
+content.each do |row|
+  name = row[:first_name]
+  zipcode = format_zipcode(row[:zipcode], 5)
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
+end
